e089c39a7dd16c414a16ee1f7be9f496da53d9a1
unknown <marco.campos001@gmail.com>
2019-04-23 19:09:38 -0400

    first commit

---
+++ ifttt_request.py
@@ -0,0 +1,23 @@
+import requests
+import config
+
+
+def send_ifttt_request(event):
+    """Sends event to IFTTT"""
+    maker_url = 'https://maker.ifttt.com/trigger/{0}/with/key/{1}'.format(event, config.API_KEY)
+    print("Sending request to IFTTT")
+    return requests.post(maker_url)
+
+def main():
+    event = 'vibration_stopped'
+    # event = 'washer_stopped_notification'
+    send_ifttt_request(event)
+
+
+main()
+
+
+
+
+
+
