e868327197ec3c619e8c02394f6a675f8b528cb7
unknown <marco.campos001@gmail.com>
2019-05-08 15:09:35 -0400

    adding leds

--- sensor.py
+++ sensor.py
@@ -7,16 +7,25 @@ GPIO.setmode(GPIO.BCM)
 # Input pins
 horizontal_sensor = 18
 vertical_sensor = 23
+# Output pins
+horizontal_led = 17
+vertical_led = 4
 
 GPIO.setup(horizontal_sensor, GPIO.IN)
 GPIO.setup(vertical_sensor, GPIO.IN)
+GPIO.setup(horizontal_led, GPIO.OUT)
+GPIO.setup(vertical_led, GPIO.OUT)
 
 while True:
     horizontal_state = GPIO.input(horizontal_sensor)
     vertical_state = GPIO.input(vertical_sensor)
-    # print ('Sensors state: horizontal: {0}, vertical: {1}'.format(horizontal_state, vertical_state))
+    print ('Sensors state: horizontal: {0}, vertical: {1}'.format(horizontal_state, vertical_state))
     sleep(0.5)
     if horizontal_state:
-        print 'Horizontal vibration'
+        # print 'Horizontal vibration'
+        GPIO.output(horizontal_led, True)
+
     if vertical_state:
-        print 'Vertical vibration'
+        # print 'Vertical vibration'
+        GPIO.output(vertical_led, True)
+
