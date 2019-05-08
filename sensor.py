fcb7fa336b4d5a8ee805fc7fb218f2d9a4e1a09d
Marco Campos <marco.campos001@gmail.com>
2019-05-08 14:26:04 -0400

    vibration sensor working

--- sensor.py
+++ sensor.py
@@ -6,17 +6,21 @@ GPIO.setmode(GPIO.BCM)
 
 # Input pins
 # forward_pin = 12
-backward_pin = 26
+backward_pin = 18
 # turn_right_pin = 13
 # turn_left_pin = 19
 
 # GPIO.setup(forward_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(backward_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(backward_pin, GPIO.IN)#, pull_up_down=GPIO.PUD_UP)
 # GPIO.setup(turn_right_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # GPIO.setup(turn_left_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
+    state = GPIO.input(backward_pin)
+    print state
+    sleep(1)
+    if state == True:
     # forward_state = GPIO.input(forward_pin)
-    backward_state = GPIO.input(backward_pin)
-    if backward_state == False:
+#    backward_state = GPIO.input(backward_pin)
+ #   if backward_state == True:
         print 'Vibration detected'
