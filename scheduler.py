51c5b509f4a7a4d7180a2f5d6d0c96ae1856b615
unknown <marco.campos001@gmail.com>
2019-05-08 18:43:21 -0400

    cleaning up

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,15 @@
-import sched, time
-# from time import sleep, time
 from datetime import datetime, timedelta
 import pandas as pd
+# import random
+# import sched, time
 
 start_time = datetime.now()
 print start_time
-sleep_time = 0.5
+sleep_time = 1
 
 hor_cols = ['datetime', 'horizontal']
 ver_cols = ['datetime', 'vertical']
-s = sched.scheduler(time.time, time.sleep)
+
 
 def define_df():
     horizontal_df = pd.DataFrame(columns=hor_cols)
@@ -26,22 +26,19 @@ def current_df(horizontal_df, horizontal_state, vertical_df, vertical_state):
 
 
 def merge_csv(horizontal_df, vertical_df):
-    result_df = pd.merge(horizontal_df, vertical_df, on='datetime', how='outer')
+    result_df = pd.merge(horizontal_df, vertical_df, on='datetime', how='outer').sort_values(by='datetime')
+    # result_df = result_df.sort_values('datetime')
     print result_df
 
-horizontal_df, vertical_df = define_df()
-
-while True:
-    horizontal_state = '0'
-    # horizontal_list = pd.DataFrame([[datetime.now().strftime('%H:%M:%S'), horizontal_state]], columns=hor_cols)
-    # horizontal_df = horizontal_df.append(horizontal_list)
-    vertical_state = '1'
-    # vertical_list = pd.DataFrame([[datetime.now().strftime('%H:%M:%S'), vertical_state]], columns=ver_cols)
-    # vertical_df = vertical_df.append(vertical_list)
-    horizontal_df, vertical_df = current_df(horizontal_df, horizontal_state, vertical_df, vertical_state)
-    time.sleep(sleep_time)
-
-    if (datetime.now() - start_time) > timedelta(seconds=10):
-        start_time = datetime.now()
-        merge_csv(horizontal_df, vertical_df)
-        # s.enter(20, 1, merge_csv(horizontal_df, vertical_df),  (s,))
+# horizontal_df, vertical_df = define_df()
+# while True:
+#     horizontal_state = random.randint(0, 1)
+#     vertical_state = random.randint(0, 1)
+#     horizontal_df, vertical_df = current_df(horizontal_df, horizontal_state, vertical_df, vertical_state)
+#
+#     if (datetime.now() - start_time) > timedelta(seconds=5):
+#         start_time = datetime.now()
+#         merge_csv(horizontal_df, vertical_df)
+#
+#     time.sleep(sleep_time)
+#         # s.enter(20, 1, merge_csv(horizontal_df, vertical_df),  (s,))
--- sensor.py
+++ sensor.py
@@ -24,14 +24,8 @@ GPIO.setup(horizontal_led, GPIO.OUT)
 GPIO.setup(vertical_led, GPIO.OUT)
 
 hor_cols = ['datetime', 'horizontal']
-# horizontal_df = pd.DataFrame(columns=hor_cols)
 ver_cols = ['datetime', 'vertical']
-# vertical_df = pd.DataFrame(columns=ver_cols)
 horizontal_df, vertical_df = scheduler.define_df()
-
-# def merge_csv(horizontal_df, vertical_df):
-#     result_df = pd.merge(horizontal_df, vertical_df, on='datetime', how='outer')
-#     print result_df
 
 while True:
     horizontal_state = GPIO.input(horizontal_sensor)
@@ -39,22 +33,19 @@ while True:
     GPIO.output(horizontal_led, False)
     GPIO.output(vertical_led, False)
     print ('Sensors state: horizontal: {0}, vertical: {1}'.format(horizontal_state, vertical_state))
+    horizontal_df, vertical_df = scheduler.current_df(horizontal_df, horizontal_state, vertical_df, vertical_state)
 
     if horizontal_state or vertical_state:
         if horizontal_state:
             GPIO.output(horizontal_led, True)
         elif vertical_state:
             GPIO.output(vertical_led, True)
-        # horizontal_list = pd.DataFrame([[datetime.now().strftime('%H:%M:%S'), horizontal_state]], columns=hor_cols)
-        # horizontal_df = horizontal_df.append(horizontal_list)
-        # vertical_list = pd.DataFrame([[datetime.now().strftime('%H:%M:%S'), vertical_state]], columns=ver_cols)
-        # vertical_df = vertical_df.append(vertical_list)
-        horizontal_df, vertical_df = scheduler.current_df(horizontal_df, horizontal_state, vertical_df, vertical_state)
-    time.sleep(sleep_time)
 
     if (datetime.now() - start_time) > timedelta(seconds=10):
         start_time = datetime.now()
         scheduler.merge_csv(horizontal_df, vertical_df)
+
+    time.sleep(sleep_time)
 
 
 
